db38a011e6942b4c6ea1127d8cda38df81fedfe7
Kayckyy <ckayck414@gmail.com>
2026-05-03 21:11:40 -0300

    Update resampler.py

--- engine/resampler.py
+++ engine/resampler.py
@@ -1,1 +1,23 @@
+import numpy as np
+from scipy.signal import resample_poly
+from math import gcd
 
+
+class Resampler:
+    TARGET_SR = 44100
+
+    def resample(self, audio: np.ndarray, sr: int) -> np.ndarray:
+        if sr == self.TARGET_SR:
+            return audio
+
+        common = gcd(sr, self.TARGET_SR)
+        up = self.TARGET_SR // common
+        down = sr // common
+
+        if audio.ndim == 1:
+            return resample_poly(audio, up, down).astype(np.float32)
+
+        # Stereo: processa cada canal
+        l = resample_poly(audio[:, 0], up, down)
+        r = resample_poly(audio[:, 1], up, down)
+        return np.stack([l, r], axis=1).astype(np.float32)
