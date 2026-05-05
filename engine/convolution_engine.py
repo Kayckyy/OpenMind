a84af577a14c1ee9ee070c5013cdf0d7fda06b6e
Kayckyy <ckayck414@gmail.com>
2026-05-05 12:39:42 -0300

    Refactor ConvolutionEngine to use a dictionary for IRs

--- engine/convolution_engine.py
+++ engine/convolution_engine.py
@@ -1,32 +1,44 @@
 import numpy as np
+from engine.upmix import Upmix
 
 
 class ConvolutionEngine:
-    def __init__(self, ir_ll, ir_lr, ir_rl, ir_rr):
-        self.ir_ll = ir_ll
-        self.ir_lr = ir_lr
-        self.ir_rl = ir_rl
-        self.ir_rr = ir_rr
+    def __init__(self, ir_front_l, ir_front_r,
+                       ir_rear_l,  ir_rear_r,
+                       ir_side_ll, ir_side_lr,
+                       ir_side_rl, ir_side_rr):
+        self.irs = {
+            'front_l':  ir_front_l,
+            'front_r':  ir_front_r,
+            'rear_l':   ir_rear_l,
+            'rear_r':   ir_rear_r,
+            'side_ll':  ir_side_ll,
+            'side_lr':  ir_side_lr,
+            'side_rl':  ir_side_rl,
+            'side_rr':  ir_side_rr,
+        }
+        self.upmix = Upmix()
         self._init_overlaps()
 
     def _init_overlaps(self):
-        ir_len = max(len(self.ir_ll), len(self.ir_lr),
-                     len(self.ir_rl), len(self.ir_rr))
-        self._ov_ll = np.zeros(ir_len - 1, dtype=np.float32)
-        self._ov_lr = np.zeros(ir_len - 1, dtype=np.float32)
-        self._ov_rl = np.zeros(ir_len - 1, dtype=np.float32)
-        self._ov_rr = np.zeros(ir_len - 1, dtype=np.float32)
+        ir_len = max(len(ir) for ir in self.irs.values())
+        self._overlaps = {
+            k: np.zeros(ir_len - 1, dtype=np.float32)
+            for k in self.irs
+        }
 
     def reset(self):
         self._init_overlaps()
 
-    def _overlap_add(self, signal, ir, overlap):
+    def _overlap_add(self, signal: np.ndarray,
+                     ir: np.ndarray,
+                     overlap: np.ndarray) -> np.ndarray:
         ir_len = len(ir)
         fft_size = 1
         while fft_size < len(signal) + ir_len - 1:
             fft_size <<= 1
 
-        ir_fft = np.fft.rfft(ir, n=fft_size)
+        ir_fft    = np.fft.rfft(ir, n=fft_size)
         block_fft = np.fft.rfft(signal, n=fft_size)
         conv = np.fft.irfft(block_fft * ir_fft, n=fft_size)
         conv = conv[:len(signal) + ir_len - 1].astype(np.float32)
@@ -37,13 +49,22 @@ class ConvolutionEngine:
 
         return conv[:len(signal)]
 
-    def process(self, input_l, input_r):
-        ll = self._overlap_add(input_l, self.ir_ll, self._ov_ll)
-        lr = self._overlap_add(input_l, self.ir_lr, self._ov_lr)
-        rl = self._overlap_add(input_r, self.ir_rl, self._ov_rl)
-        rr = self._overlap_add(input_r, self.ir_rr, self._ov_rr)
+    def process(self, input_l: np.ndarray,
+                      input_r: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+        channels = self.upmix.process(input_l, input_r)
 
-        out_l = (ll + rl) * 0.5
-        out_r = (lr + rr) * 0.5
+        # Convolui cada canal com seu IR
+        front_l = self._overlap_add(channels['front'],  self.irs['front_l'],  self._overlaps['front_l'])
+        front_r = self._overlap_add(channels['front'],  self.irs['front_r'],  self._overlaps['front_r'])
+        rear_l  = self._overlap_add(channels['rear'],   self.irs['rear_l'],   self._overlaps['rear_l'])
+        rear_r  = self._overlap_add(channels['rear'],   self.irs['rear_r'],   self._overlaps['rear_r'])
+        side_ll = self._overlap_add(channels['side_l'], self.irs['side_ll'],  self._overlaps['side_ll'])
+        side_lr = self._overlap_add(channels['side_l'], self.irs['side_lr'],  self._overlaps['side_lr'])
+        side_rl = self._overlap_add(channels['side_r'], self.irs['side_rl'],  self._overlaps['side_rl'])
+        side_rr = self._overlap_add(channels['side_r'], self.irs['side_rr'],  self._overlaps['side_rr'])
+
+        # Mix binaural final
+        out_l = (front_l + rear_l + side_ll + side_rl) * 0.35
+        out_r = (front_r + rear_r + side_lr + side_rr) * 0.35
 
         return out_l.astype(np.float32), out_r.astype(np.float32)
