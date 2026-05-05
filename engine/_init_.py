7e12f35658e8a1f8d862045530e2a9bec35ad057
Kayckyy <ckayck414@gmail.com>
2026-05-05 12:50:00 -0300

    Add Upmix to module exports

--- engine/_init_.py
+++ engine/_init_.py
@@ -1,5 +1,6 @@
 from .ir_loader import IrLoader
 from .resampler import Resampler
 from .convolution_engine import ConvolutionEngine
+from .upmix import Upmix
 
-__all__ = ["IrLoader", "Resampler", "ConvolutionEngine"]
+__all__ = ["IrLoader", "Resampler", "ConvolutionEngine", "Upmix"]
