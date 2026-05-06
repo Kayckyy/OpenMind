3cee74855af61085a89f50393525dfc01dbca839
Kayckyy <ckayck414@gmail.com>
2026-05-06 16:33:31 -0300

    Replace Upmix with BandRouter in __all__ exports

--- engine/_init_.py
+++ engine/_init_.py
@@ -1,6 +1,6 @@
 from .ir_loader import IrLoader
 from .resampler import Resampler
 from .convolution_engine import ConvolutionEngine
-from .upmix import Upmix
+from .band_router import BandRouter
 
-__all__ = ["IrLoader", "Resampler", "ConvolutionEngine", "Upmix"]
+__all__ = ["IrLoader", "Resampler", "ConvolutionEngine", "BandRouter"]
