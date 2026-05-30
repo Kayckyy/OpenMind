dd9db729d330f4a0374bf253ed7e3009ef8282d0
Kayckyy <ckayck414@gmail.com>
2026-05-29 22:33:41 -0300

    Remove BandRouter from module exports

--- engine/_init_.py
+++ engine/_init_.py
@@ -1,6 +1,5 @@
 from .ir_loader import IrLoader
 from .resampler import Resampler
 from .convolution_engine import ConvolutionEngine
-from .band_router import BandRouter
 
-__all__ = ["IrLoader", "Resampler", "ConvolutionEngine", "BandRouter"]
+__all__ = ["IrLoader", "Resampler", "ConvolutionEngine"]
